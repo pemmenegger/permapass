484a9b967be6f63de519c3ab290bd88725f22d0b
Pascal Emmenegger <pascalemmenegger@hotmail.ch>
2024-05-02 17:27:24 +0200

    app reset, starting from scratch connecting components

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,31 +1,6 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { lazyImports: true }]],
-    plugins: [
-      "expo-router/babel",
-      "@babel/transform-react-jsx-source",
-      [`@babel/plugin-transform-private-methods`, { loose: true }],
-      "babel-plugin-transform-typescript-metadata",
-      [
-        "module-resolver",
-        {
-          alias: {
-            "~": "./src",
-          },
-        },
-      ],
-      "@babel/plugin-syntax-import-assertions",
-      [
-        "babel-plugin-rewrite-require",
-        {
-          aliases: {
-            crypto: "crypto-browserify",
-            stream: "stream-browserify",
-          },
-        },
-      ],
-      // 'react-native-reanimated/plugin',
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
--- app/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-import config from "./config";
-
-export const fromArweaveTxidToPassportMetadataURL = (txid: string) => {
-  return config.BASE_URI_SCHEME + `/read?arweaveTxid=${txid}`;
-};
