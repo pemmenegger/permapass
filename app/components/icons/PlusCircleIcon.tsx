9f7e743df666cfd9303a8d8152dad42844db80b3
Pascal Emmenegger <pascalemmenegger@hotmail.ch>
2024-05-21 17:58:18 +0200

    icons fixed

--- app/components/icons/PlusCircleIcon.tsx
+++ app/components/icons/PlusCircleIcon.tsx
@@ -18,8 +18,8 @@ export const PlusCircleIcon = (props: IconProps) => {
         d="M11 7V15M7 11H15M21 11C21 16.5228 16.5228 21 11 21C5.47715 21 1 16.5228 1 11C1 5.47715 5.47715 1 11 1C16.5228 1 21 5.47715 21 11Z"
         stroke={props.color}
         strokeWidth={props.strokeWidth || 1}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </Svg>
   );
