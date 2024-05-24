9a30f8cb078aa340608e0e5b42e3eb223a1eff88
Pascal Emmenegger <pascalemmenegger@hotmail.ch>
2024-05-24 14:52:07 +0200

    reading passports design implemented

--- app/app/read.tsx
+++ app/app/read.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { usePassportMetadata } from "../hooks/usePassportMetadata";
 import { useReadQueryParams } from "../hooks/useReadQueryParams";
 import { usePassportHistory } from "../hooks/usePassportHistory";
@@ -8,36 +8,95 @@ import { useNFTRegistry } from "../hooks/blockchain/useNFTRegistry";
 import { useDIDRegistry } from "../hooks/blockchain/useDIDRegistry";
 import { Passport, PassportMetadata } from "../types";
 import ViewWithHeader from "../components/ViewWithHeader";
+import { SecondaryButton } from "../components/ui/buttons";
+import StepTitle from "../components/stepper/StepTitle";
+import { commonColors, commonStyles } from "../styles";
 
-const MetadataDisplay = ({
-  isLoading,
-  error,
-  metadata,
-}: {
+interface MetadataDisplayProps {
   isLoading: boolean;
   error?: string;
   metadata?: PassportMetadata;
-}) => (
-  <>
-    {isLoading && <Text>Loading metadata...</Text>}
-    {error && <Text>Error Metadata: {error}</Text>}
-    <Text>Metadata:</Text>
-    <Text>{JSON.stringify(metadata, null, 2)}</Text>
-  </>
-);
+}
 
-const HistoryDisplay = ({ isLoading, error, history }: { isLoading: boolean; error?: string; history: Passport[] }) => (
-  <>
-    {isLoading && <Text>Loading passport...</Text>}
-    {error && <Text>Error Passport: {error}</Text>}
-    <Text>History:</Text>
-    {history.length === 0 ? (
-      <Text>No history found</Text>
-    ) : (
-      history.map((passport, index) => <Text key={index}>{JSON.stringify(passport, null, 2)}</Text>)
-    )}
-  </>
-);
+const MetadataDisplay = ({ isLoading, error, metadata }: MetadataDisplayProps) => {
+  if (isLoading) {
+    return <Text>Loading metadata...</Text>;
+  }
+
+  if (error) {
+    return <Text>Error Metadata: {error}</Text>;
+  }
+
+  if (!metadata) {
+    return <Text>No metadata available</Text>;
+  }
+
+  const renderMetadataContent = () => {
+    switch (metadata.type) {
+      case "nft":
+        return (
+          <>
+            <Text style={commonStyles.h4}>Token Id: {metadata.tokenId.toString()}</Text>
+            <Text style={commonStyles.h4}>NFT Registry Address: {metadata.address}</Text>
+            <Text style={commonStyles.h4}>Blockchain: {metadata.chainId}</Text>
+          </>
+        );
+      case "pbt":
+        return (
+          <>
+            <Text style={commonStyles.h4}>PBT Registry Address: {metadata.address}</Text>
+            <Text style={commonStyles.h4}>Blockchain: {metadata.chainId}</Text>
+          </>
+        );
+      case "did":
+        return (
+          <>
+            <Text style={commonStyles.h4}>DID: {metadata.did}</Text>
+            <Text style={commonStyles.h4}>DID Registry Address: {metadata.address}</Text>
+            <Text style={commonStyles.h4}>Blockchain: {metadata.chainId}</Text>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return <View style={{ marginBottom: 20 }}>{renderMetadataContent()}</View>;
+};
+
+interface HistoryDisplayProps {
+  isLoading: boolean;
+  error?: string;
+  history: Passport[];
+  update: () => void;
+}
+
+const HistoryDisplay = ({ isLoading, error, history, update }: HistoryDisplayProps) => {
+  if (isLoading) {
+    return <Text>Loading passport...</Text>;
+  }
+
+  if (error) {
+    return <Text>Error Passport: {error}</Text>;
+  }
+
+  const currentPassport = history[0];
+  if (!currentPassport) {
+    return <Text>No passport available</Text>;
+  }
+
+  return (
+    <View style={styles.passportContainer}>
+      {Object.entries(currentPassport).map(([key, value]) => (
+        <View key={key} style={styles.passportAttribute}>
+          <Text style={styles.passportAttributeName}>{key}</Text>
+          <Text style={styles.passportAttributeValue}>{value}</Text>
+        </View>
+      ))}
+      <SecondaryButton title="Update" onPress={update} />
+    </View>
+  );
+};
 
 export default function Page() {
   const { didRegistry } = useDIDRegistry();
@@ -46,7 +105,6 @@ export default function Page() {
   const [version, setVersion] = useState(0);
 
   const { passportMetadata, isLoading: isMetadataLoading, error: metadataError } = usePassportMetadata({ metadataURI });
-
   const {
     passportHistory,
     isLoading: isPassportHistoryLoading,
@@ -82,10 +140,44 @@ export default function Page() {
 
   return (
     <ViewWithHeader>
+      {passportMetadata && (
+        <StepTitle
+          text={`You have read a ${passportMetadata.type.toUpperCase()}-based passport.`}
+          highlight={`${passportMetadata.type.toUpperCase()}-based`}
+        />
+      )}
       <MetadataDisplay isLoading={isMetadataLoading} error={metadataError} metadata={passportMetadata} />
-      <Text>-----</Text>
-      <HistoryDisplay isLoading={isPassportHistoryLoading} error={passportHistoryError} history={passportHistory} />
-      <Button title="Update" onPress={update} />
+      {passportMetadata && (
+        <HistoryDisplay
+          isLoading={isPassportHistoryLoading}
+          error={passportHistoryError}
+          history={passportHistory}
+          update={update}
+        />
+      )}
     </ViewWithHeader>
   );
 }
+
+const styles = StyleSheet.create({
+  passportContainer: {
+    borderWidth: 1,
+    borderColor: commonColors.secondary,
+    backgroundColor: commonColors.white,
+    paddingVertical: 16,
+    paddingHorizontal: 12,
+    borderRadius: 10,
+  },
+  passportAttribute: {
+    marginBottom: 20,
+  },
+  passportAttributeName: {
+    fontSize: 12,
+    fontFamily: "Inter-SemiBold",
+    textTransform: "uppercase",
+  },
+  passportAttributeValue: {
+    fontSize: 32,
+    color: commonColors.gray,
+  },
+});
