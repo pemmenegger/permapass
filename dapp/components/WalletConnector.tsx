a8f7cb88a27364ab69e093add7044de3e4e3d990
Pascal Emmenegger <pascalemmenegger@hotmail.ch>
2024-06-09 22:07:38 +0200

    basic address works

--- dapp/components/WalletConnector.tsx
+++ dapp/components/WalletConnector.tsx
@@ -3,6 +3,14 @@ import { useWeb3Modal } from "@web3modal/wagmi-react-native";
 import { Pressable, View, StyleSheet, Text } from "react-native";
 import { useWalletClient } from "wagmi";
 import { commonColors } from "../styles";
+// import { WalletIcon } from "./icons/WalletIcon";
+
+const InfoBlock = ({ label, value }: { label: string; value: string }) => (
+  <View>
+    <Text style={styles.subtext}>{label}</Text>
+    <Text style={styles.text}>{value}</Text>
+  </View>
+);
 
 export default function WalletConnector() {
   const { open } = useWeb3Modal();
@@ -10,12 +18,17 @@ export default function WalletConnector() {
 
   return (
     <Pressable style={styles.button} onPress={async () => await open()}>
-      {walletClient?.chain ? (
+      {walletClient ? (
         <View style={styles.isConnectedContainer}>
-          <Text style={styles.text}>{walletClient?.chain.name}</Text>
+          <InfoBlock label="Address" value={walletClient.account.address} />
+          {/* <InfoBlock label={data?.symbol || ""} value={formattedBalance} /> */}
+          <InfoBlock label="Network" value={walletClient.chain.name} />
         </View>
       ) : (
         <View style={styles.isDisconnectedContainer}>
+          {/* <View style={styles.icon}>
+            <WalletIcon height={15} strokeWidth={1.2} color={commonColors.black} />
+          </View> */}
           <Text style={styles.text}>Connect Wallet</Text>
         </View>
       )}
@@ -25,12 +38,12 @@ export default function WalletConnector() {
 
 const styles = StyleSheet.create({
   button: {
-    borderRadius: 32,
+    borderRadius: 12,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    borderWidth: 1,
     backgroundColor: commonColors.primary,
-    marginVertical: 12,
-    marginHorizontal: 12,
-    paddingVertical: 12,
-    paddingHorizontal: 16,
+    borderColor: commonColors.secondary,
   },
   isConnectedContainer: {
     flexDirection: "row",
@@ -42,8 +55,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  subtext: {
+    fontFamily: "Inter-SemiBold",
+    letterSpacing: -0.1,
+    fontSize: 12,
+    color: commonColors.secondary,
+  },
   text: {
-    color: commonColors.white,
-    fontSize: 16,
+    fontFamily: "Inter-Medium",
+    letterSpacing: -0.2,
+    fontSize: 14,
+    color: commonColors.black,
+  },
+  icon: {
+    marginRight: 8,
   },
 });
