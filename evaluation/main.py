ce7080305bd2787ce567a4dc7079c95d3e0b7f71
Pascal Emmenegger <pascalemmenegger@hotmail.ch>
2024-06-14 14:31:59 +0200

    plots generation refactored

--- evaluation/main.py
+++ evaluation/main.py
@@ -1,28 +1,48 @@
-import os
-
-from utils import load_json, plot_durations, process_registry_data
+from utils import load_json, plot_durations, plot_gas_used, process_registry_data
 
 
-def main():
-    # Plot Arweave Upload Durations
-    arweave_data = load_json("data/ArweaveUpload.json")
+def plot_arweave():
+    arweave_data = load_json("data/Arweave.json")
     plot_durations(
         data_list=[arweave_data["create"], arweave_data["update"]],
         labels=["passport-create.json", "passport-update.json"],
         title="Arweave Upload Durations via Irys Node 2",
-        output_path=os.path.join("plots"),
-        output_filename="arweave_upload.png",
+        output_path="plots",
+        output_filename="arweave.png",
         x_axis="ms",
     )
 
-    did_durations = {
-        "deployment": [],
-        "create": [],
-        "read": [],
-        "update": [],
-        "delete": [],
+
+def plot_halo_nfc_metadata_registry():
+    labels = {
+        "deployment": "Deployment",
+        "create": "Create",
+        "read": "Read",
     }
-    labels_template = {
+
+    data = process_registry_data("data/contracts/HaloNFCMetadataRegistry.json")
+    title = "Halo NFC Metadata Registry"
+    output_filename_prefix = "halo_nfc_metadata_registry"
+
+    plot_durations(
+        data_list=[data[key] for key in data.keys()],
+        labels=labels.values(),
+        title=title,
+        output_path="plots",
+        output_filename=f"{output_filename_prefix}_durations.png",
+        x_axis="s",
+    )
+    plot_gas_used(
+        data_list=[data[key] for key in data.keys()],
+        labels=labels.values(),
+        title=title,
+        output_path="plots",
+        output_filename=f"{output_filename_prefix}_gas_used.png",
+    )
+
+
+def plot_digital_identifier_contracts():
+    labels = {
         "deployment": "Deployment",
         "create": "Create",
         "read": "Read",
@@ -30,146 +50,88 @@ def main():
         "delete": "Delete",
     }
 
-    # Plot DID Registry Durations
-    did_registry_data = load_json("data/contracts/DIDRegistry.json")
-    process_registry_data(did_registry_data, did_durations)
-    plot_durations(
-        data_list=[did_durations[key] for key in did_durations.keys()],
-        labels=[labels_template[key] for key in did_durations.keys()],
-        title="DID Registry Durations",
-        output_path=os.path.join("plots"),
-        output_filename="did_registry_durations.png",
-        x_axis="s",
-    )
+    registry_configs = [
+        {
+            "title": "DID Registry",
+            "output_filename_prefix": "did_registry",
+            "data": process_registry_data("data/contracts/DIDRegistry.json"),
+        },
+        {
+            "title": "NFT Registry",
+            "output_filename_prefix": "nft_registry",
+            "data": process_registry_data("data/contracts/NFTRegistry.json"),
+        },
+        {
+            "title": "PBT Registry",
+            "output_filename_prefix": "pbt_registry",
+            "data": process_registry_data("data/contracts/PBTRegistry.json"),
+        },
+    ]
 
-    nft_durations = {
-        "deployment": [],
-        "create": [],
-        "read": [],
-        "update": [],
-        "delete": [],
-    }
-    labels_template = {
-        "deployment": "Deployment",
-        "create": "Create",
-        "read": "Read",
-        "update": "Update",
-        "delete": "Delete",
-    }
+    for config in registry_configs:
+        registry_data = config["data"]
+        plot_durations(
+            data_list=[registry_data[key] for key in registry_data.keys()],
+            labels=labels.values(),
+            title=config["title"],
+            output_path="plots",
+            output_filename=f'{config["output_filename_prefix"]}_durations.png',
+            x_axis="s",
+        )
+        plot_gas_used(
+            data_list=[registry_data[key] for key in registry_data.keys()],
+            labels=labels.values(),
+            title=config["title"],
+            output_path="plots",
+            output_filename=f'{config["output_filename_prefix"]}_gas_used.png',
+        )
 
-    # Plot NFT Registry Durations
-    nft_registry_data = load_json("data/contracts/NFTRegistry.json")
-    process_registry_data(nft_registry_data, nft_durations)
-    plot_durations(
-        data_list=[nft_durations[key] for key in nft_durations.keys()],
-        labels=[labels_template[key] for key in nft_durations.keys()],
-        title="NFT Registry Durations",
-        output_path=os.path.join("plots"),
-        output_filename="nft_registry_durations.png",
-        x_axis="s",
-    )
-
-    pbt_durations = {
-        "deployment": [],
-        "create": [],
-        "read": [],
-        "update": [],
-        "delete": [],
-    }
-    labels_template = {
-        "deployment": "Deployment",
-        "create": "Create",
-        "read": "Read",
-        "update": "Update",
-        "delete": "Delete",
-    }
-
-    # Plot PBT Registry Durations
-    pbt_registry_data = load_json("data/contracts/PBTRegistry.json")
-    process_registry_data(pbt_registry_data, pbt_durations)
-    plot_durations(
-        data_list=[pbt_durations[key] for key in pbt_durations.keys()],
-        labels=[labels_template[key] for key in pbt_durations.keys()],
-        title="PBT Registry Durations",
-        output_path=os.path.join("plots"),
-        output_filename="pbt_registry_durations.png",
-        x_axis="s",
-    )
-
-    deployment_durations = {
-        "DID Registry": did_durations["deployment"],
-        "NFT Registry": nft_durations["deployment"],
-        "PBT Registry": pbt_durations["deployment"],
+    combined_registry_data = {
+        "deployment": {
+            config["title"]: config["data"]["deployment"] for config in registry_configs
+        },
+        "create": {
+            config["title"]: config["data"]["create"] for config in registry_configs
+        },
+        "read": {
+            config["title"]: config["data"]["read"] for config in registry_configs
+        },
+        "update": {
+            config["title"]: config["data"]["update"] for config in registry_configs
+        },
+        "delete": {
+            config["title"]: config["data"]["delete"] for config in registry_configs
+        },
     }
 
-    plot_durations(
-        data_list=[deployment_durations[key] for key in deployment_durations.keys()],
-        labels=deployment_durations.keys(),
-        title="Registry Deployments Durations",
-        output_path=os.path.join("plots"),
-        output_filename="registry_deployments_durations.png",
-        x_axis="s",
-    )
+    for action_type in combined_registry_data:
+        plot_durations(
+            data_list=[
+                combined_registry_data[action_type][key]
+                for key in combined_registry_data[action_type].keys()
+            ],
+            labels=combined_registry_data[action_type].keys(),
+            title=f"Registry {action_type.capitalize()}",
+            output_path="plots",
+            output_filename=f"combined_durations_{action_type}.png",
+            x_axis="s",
+        )
+        plot_gas_used(
+            data_list=[
+                combined_registry_data[action_type][key]
+                for key in combined_registry_data[action_type].keys()
+            ],
+            labels=combined_registry_data[action_type].keys(),
+            title=f"Registry {action_type.capitalize()}",
+            output_path="plots",
+            output_filename=f"combined_gas_used_{action_type}.png",
+        )
 
-    create_durations = {
-        "DID Registry": did_durations["create"],
-        "NFT Registry": nft_durations["create"],
-        "PBT Registry": pbt_durations["create"],
-    }
 
-    plot_durations(
-        data_list=[create_durations[key] for key in create_durations.keys()],
-        labels=create_durations.keys(),
-        title="Registry Create Durations",
-        output_path=os.path.join("plots"),
-        output_filename="registry_create_durations.png",
-        x_axis="s",
-    )
-
-    read_durations = {
-        "DID Registry": did_durations["read"],
-        "NFT Registry": nft_durations["read"],
-        "PBT Registry": pbt_durations["read"],
-    }
-
-    plot_durations(
-        data_list=[read_durations[key] for key in read_durations.keys()],
-        labels=read_durations.keys(),
-        title="Registry Read Durations",
-        output_path=os.path.join("plots"),
-        output_filename="registry_read_durations.png",
-        x_axis="s",
-    )
-
-    update_durations = {
-        "DID Registry": did_durations["update"],
-        "NFT Registry": nft_durations["update"],
-        "PBT Registry": pbt_durations["update"],
-    }
-
-    plot_durations(
-        data_list=[update_durations[key] for key in update_durations.keys()],
-        labels=update_durations.keys(),
-        title="Registry Update Durations",
-        output_path=os.path.join("plots"),
-        output_filename="registry_update_durations.png",
-        x_axis="s",
-    )
-
-    delete_durations = {
-        "DID Registry": did_durations["delete"],
-        "NFT Registry": nft_durations["delete"],
-        "PBT Registry": pbt_durations["delete"],
-    }
-
-    plot_durations(
-        data_list=[delete_durations[key] for key in delete_durations.keys()],
-        labels=delete_durations.keys(),
-        title="Registry Delete Durations",
-        output_path=os.path.join("plots"),
-        output_filename="registry_delete_durations.png",
-        x_axis="s",
-    )
+def main():
+    plot_arweave()
+    plot_halo_nfc_metadata_registry()
+    plot_digital_identifier_contracts()
 
 
 if __name__ == "__main__":
--- evaluation/utils.py
+++ evaluation/utils.py
@@ -5,6 +5,43 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+def load_json(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+
+def process_registry_data(data_path):
+    all_registry_data = load_json(data_path)
+
+    registry_actions = {
+        "deployment": [],
+        "create": [],
+        "read": [],
+        "update": [],
+        "delete": [],
+    }
+
+    for record in all_registry_data["11155111"]:
+        for key in registry_actions.keys():
+            if not record[key]:
+                continue
+            elif len(record[key]) == 1:
+                registry_actions[key].extend(record[key])
+            else:
+                gas_used_sum = sum(item["gasUsedInWei"] for item in record[key][1:])
+                duration_sum = sum(item["durationInMs"] for item in record[key][1:])
+                start_timestamp = record[key][0]["startTimestamp"]
+                registry_actions[key].append(
+                    {
+                        "gasUsedInWei": gas_used_sum,
+                        "durationInMs": duration_sum,
+                        "startTimestamp": start_timestamp,
+                    }
+                )
+
+    return registry_actions
+
+
 def plot_durations(
     data_list,
     labels,
@@ -50,7 +87,7 @@ def plot_durations(
 
     plt.xlabel("Start Time on 2024-06-13 (HH:MM CEST)")
     plt.ylabel(f"Duration ({unit})")
-    plt.title(title)
+    plt.title(f"{title} Durations")
     plt.xticks(rotation=45)
     plt.legend()
     plt.grid(True, which="both", linestyle="--", linewidth=0.5)
@@ -62,24 +99,47 @@ def plot_durations(
     plt.close()
 
 
-def load_json(file_path):
-    with open(file_path, "r") as f:
-        return json.load(f)
+def plot_gas_used(
+    data_list,
+    labels,
+    title,
+    output_path,
+    output_filename,
+):
+    plt.figure(figsize=(14, 8))
 
+    for _, (data, label) in enumerate(zip(data_list, labels)):
+        df = pd.DataFrame(data)
 
-def process_registry_data(data, durations):
-    for record in data["11155111"]:
-        for key in durations.keys():
-            if not record[key]:
-                continue
-            elif len(record[key]) == 1:
-                durations[key].extend(record[key])
-            else:
-                duration_sum = sum(item["durationInMs"] for item in record[key][1:])
-                start_timestamp = record[key][0]["startTimestamp"]
-                durations[key].append(
-                    {
-                        "startTimestamp": start_timestamp,
-                        "durationInMs": duration_sum,
-                    }
-                )
+        df["startTimestamp"] = (
+            pd.to_datetime(df["startTimestamp"], unit="ms")
+            .dt.tz_localize("UTC")
+            .dt.tz_convert("Europe/Zurich")
+        )
+        # Round down to the nearest 10 minutes
+        df["formattedTimestamp"] = (
+            df["startTimestamp"].dt.floor("10min").dt.strftime("%H:%M")
+        )
+
+        mean_gas_used = df["gasUsedInWei"].mean()
+        unit = "wei"
+
+        plt.plot(
+            df["formattedTimestamp"],
+            df["gasUsedInWei"],
+            label=f"{label} (mean: {mean_gas_used:.2f} {unit})",
+            marker="o",
+        )
+
+    plt.xlabel("Start Time on 2024-06-13 (HH:MM CEST)")
+    plt.ylabel(f"Gas Used ({unit})")
+    plt.title(f"{title} Gas Used")
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.grid(True, which="both", linestyle="--", linewidth=0.5)
+    plt.tick_params(axis="both", which="major", labelsize=10)
+
+    # Ensure the output directory exists and save the plot
+    os.makedirs(output_path, exist_ok=True)
+    plt.savefig(os.path.join(output_path, output_filename))
+    plt.close()
